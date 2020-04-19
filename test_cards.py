ee9dec1228098c43f333c340390520ab1b0aa685
Amelia Froemming <7919407+afroemming@users.noreply.github.com>
2020-04-18 21:06:28 -0500

    added ability to return card back into deck

--- test_cards.py
+++ test_cards.py
@@ -4,10 +4,17 @@ import pytest
 from copy import deepcopy
 
 def test_deck_draw_returns_card():
-    assert type(deepcopy(cards.tarot_deck).draw()) == cards.Card
+    assert type(deepcopy(cards.Deck(cards.tarot_deck)).draw()) == cards.Card
 
 def test_deck_draw_exhausts_deck():
-    deck = deepcopy(cards.tarot_deck)
+    deck = deepcopy(cards.Deck(cards.tarot_deck))
     with pytest.raises(IndexError):
         for c in range(79):
             deck.draw()
+
+def test_return_returns_card():
+    deck = deepcopy(cards.Deck(cards.tarot_deck))
+    d_card = deck.draw()
+    assert not d_card in deck.curr_deck
+    deck.return_card(0)
+    assert d_card in deck.curr_deck
