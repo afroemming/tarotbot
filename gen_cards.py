b3cc477f1c4d2fd36079ff77412fbf1783c4d7fe
Andrew Froemming <andrew@froemming.me>
2020-04-18 20:17:59 -0500

    Nicer names for minor arcana

--- gen_cards.py
+++ gen_cards.py
@@ -12,16 +12,16 @@ nums = ['01', '02', '03', '04', '05', '06', '07', '08', '09',
 # Construct string generating a Card object (see cards.py) for each minor arcana in each suit
 s = ""
 for i, j in zip(names, nums):
-    s +=f"Card('{i} of Cups', 'Cups{j}.jpg'), "
+    s +=f"Card('the {i} of Cups', 'Cups{j}.jpg'), \n"
 
 for i, j in zip(names, nums):
-    s +=f"Card('{i} of Pentacles', 'Pents{j}.jpg'), "
+    s +=f"Card('the {i} of Pentacles', 'Pents{j}.jpg '), \n"
 
 for i, j in zip(names, nums):
-    s +=f"Card('{i} of Wands', 'Wands{j}.jpg'), "
+    s +=f"Card('the {i} of Wands', 'Wands{j}.jpg'), \n"
 
 for i, j in zip(names, nums):
-    s +=f"Card('{i} of Swords', 'Swords{j}.jpg'), "
+    s +=f"Card('the {i} of Swords', 'Swords{j}.jpg'), \n"
 
 #Major Arcana
 names = ['The Fool', 'The Magician', "The High Priestess", 'The Empress',
@@ -33,6 +33,6 @@ nums = ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09',
     '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21']
 
 for i, j in zip(names, nums):
-    s +=f"Card('{i}', 'RWS_Tarot_{j}_{i}.jpg'), "
+    s +=f"Card('{i}', 'RWS_Tarot_{j}_{i}.jpg'), \n"
 
-print(s[:-2])
+print(s[:-3])
